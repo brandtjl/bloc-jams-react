db6ada7849fe2fe8640c4266e01dffc32b016c4c
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-03-14 09:37:40 -0500

    added song list

--- src/components/Album.js
+++ src/components/Album.js
@@ -8,9 +8,10 @@ class Album extends Component {
         const album = albumData.find( album => {
             return album.slug === this.props.match.params.slug
         });
-{/* now it sets state.album property to album variable which is album with matching url */}
+  //now it sets state.album property to album variable which is album with matching url
         this.state = {
-            album: album
+            album: album,
+            albums: albumData      
         };
     }
 
@@ -18,7 +19,7 @@ class Album extends Component {
         return(
             <section className="album">
                 <section id="album-info">
-                    <img id="album-cover-art" src={this.state.album.albumCover} />
+                    <img id="album-cover-art" src={this.state.album.albumCover} alt="album cover"/>
                     <div className='album-details'>  {/*why use a className for some but id for others?? */}
                         <h1 id="album-title">{this.state.album.title}</h1>
                         <h2 className="artist">{this.state.album.artist}</h2>
@@ -32,6 +33,17 @@ class Album extends Component {
                         <col id="song-duration-column" />
                     </colgroup>  
                     <tbody>
+                            {
+                            this.state.album.songs.map( (song, index) => 
+                            <tr className="album-songs" key={index}>
+                            <td>{index + 1}</td>
+                            <td>{song.title}</td>
+                            <td>{song.duration}</td>
+                            <td className="ion-play"></td>
+                            <td className="ion-pause"></td>
+                            </tr>
+                            )
+                             }
                         
                     </tbody>
                 </table>
