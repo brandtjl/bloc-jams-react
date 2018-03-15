d47572ad3ccf6a3a450f6a348576a2e114564c99
Jeni Brandt <jenibrandt@legalshieldcorp.com>
2018-03-15 12:27:27 -0500

    added currentTime and duration

--- src/components/Album.js
+++ src/components/Album.js
@@ -13,14 +13,44 @@ class Album extends Component {
         this.state = {
             album: album,
             currentSong: album.songs[0],
+            currentTime: 0,
+            duration: album.songs[0].duration,
             isPlaying: false
         };
         this.audioElement = document.createElement('audio'); //not assigned to state since that would trigger a re-render of DOM
         this.audioElement.src = album.songs[0].audioSrc; //set to the first song
     }
+    componentDidMount() {  //pre-defined React Lifecycle method
+        //this.audioElement.addEventListener('timeupdate', (e) => { //time update and durationchange are predefined HTML events
+        //    this.setState({ currentTime: this.audioElement.currentTime });
+        //});
+        //this.audioElement.addEventListener('durationchange', (e) => {
+        //    this.setState({ duration: this.audioElement.duration });
+        //});
+
+        //refactoring componentDidMount() so that the callbacks are being stored on the 'this' keyword ??
+        //in order to remove event handlers, the function specified with the addEventListener() method
+        //must be an external function
+        this.eventListeners = {
+            timeupdate: e => {
+                this.setState({ currentTime: this.audioElement.currentTime });
+            },
+            durationChange: e => {
+                this.setState({ duration: this.audioElement.duration });
+            }
+        };
+        this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+        this.audioElement.addEventListener('durationchange', this.eventListeners.durationChange);
+    }
+    componentWillUnmount() {
+        this.audioElement.src = null;
+        this.audioElement.removeEventListener('timeupdate',this.eventListeners.timeupdate);
+        this.audioElement.removeEventListener('durationChange',this.eventListeners.durationChange);
+    }
+
     play() {   //how does this work if it doesn't accept a parameter?
         this.audioElement.play(); //creates <audio> element? which executes its own play()method?
-        this.setState({ isPlaying: true });
+        this.setState({ isPlaying: true }); // this.setState as opposed to this.state.isPLaying
     }
     pause() {
         this.audioElement.pause();
@@ -55,6 +85,11 @@ class Album extends Component {
         this.setSong(newSong);
         this.play(newSong);
     }
+    handleTimeChange(e) {
+        const newTime = this.audioElement.duration * e.target.value;
+        this.audioElement.currentTime = newTime; //this currentTime is predefined property of 'audio' element
+        this.setState({ currentTime: newTime });
+    }
     render() {
         return(
             <section className="album">
@@ -88,9 +123,12 @@ class Album extends Component {
                 <PlayerBar 
                 isPlaying={this.state.isPlaying} 
                 currentSong={this.state.currentSong} 
+                currentTime={this.audioElement.currentTime}  //because these are pre-defined properties of the 'audio' element
+                duration={this.audioElement.duration}
                 handleSongClick={() => this.handleSongClick(this.state.currentSong)}
                 handlePrevClick={() => this.handlePrevClick()}
                 handleNextClick={() => this.handleNextClick()}
+                handleTimeChange={(e) => this.handleTimeChange(e)}
                 /> 
             </section>
         );
